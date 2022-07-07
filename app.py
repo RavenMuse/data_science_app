0de08c0b938c3d9696a58cc5f9e70ade4721756e
hanyue <>
2022-07-07 12:05:00 +0000

    【新增】重构登录校验

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from faulthandler import disable
-
-import streamlit as st
-import streamlit_authenticator as stauth
 import os
+import warnings
+import sqlparse
 import pandas as pd
 import streamlit as st
+import streamlit_authenticator as stauth
+import streamlit.components.v1 as components
+import plotly.figure_factory as ff
+
 from pandasql import sqldf
+from datetime import datetime, timedelta
+from authenticate import Authenticate
 from tools.feature_tools import FeatureTools
 from tools.stats_tools import StatsTools
-import plotly.figure_factory as ff
-import warnings
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
@@ -19,11 +21,9 @@ class DataToolsApp:
     def __init__(self):
         self.__tools_set = []
         self.__credentials = {
-            'usernames': {
-                'admin': {
-                    'name': 'admin',
-                    'password': stauth.Hasher(['hmyzch']).generate()[0]
-                }
+            'admin': {
+                'name': 'admin',
+                'password': stauth.Hasher(['hmyzch']).generate()[0]
             }
         }
         self.__authentication_status = None
@@ -38,40 +38,68 @@ class DataToolsApp:
                 'About': "# This is a header. This is an *extremely* cool app!"
             })
 
-    def authenticate(self):
-        ### https://github.com/mkhorasani/Streamlit-Authenticator
-        self.__authenticator = stauth.Authenticate(self.__credentials,
-                                                   'some_cookie_name',
-                                                   'some_signature_key')
-        name, self.__authentication_status, username = self.__authenticator.login(
-            'Login', 'main')
+    def __login(self):
+        self.__authenticator = Authenticate(self.__credentials,
+                                            'data_science_app',
+                                            'data_science_app')
+        _, self.__authentication_status, _ = self.__authenticator.login()
+        if self.__authentication_status == False:
+            st.error('用户名/密码不正确')
+            return False
+        if self.__authentication_status == None:
+            st.info('请输入用户名/密码')
+            return False
+        return True
+
+    def __get_data(self):
+        data_panel = st.expander("数据源", True)
+        path = data_panel.file_uploader('选择一个数据文件', type='csv')
+
+        @st.cache(allow_output_mutation=True)
+        def __load_data(path=None):
+            return pd.read_csv(path)
+
+        if not path:
+            orgin_data = __load_data('/data/data_science_app/test_sample.csv')
+        else:
+            orgin_data = __load_data(path)
+
+        def __sql_format():
+            st.session_state.sql_input = sqlparse.format(
+                st.session_state.sql_input,
+                reindent=True,
+                keyword_case='upper')
+
+        sql = data_panel.text_area('SQL',
+                                   "SELECT *\nFROM orgin_data\nLIMIT 20",
+                                   key='sql_input',
+                                   on_change=__sql_format)
+        try:
+            data = sqldf(sql, locals())
+        except:
+            st.error('无效的查询!')
+            return None
+        data_panel.write(data)
+        return data
 
     def add_tool(self, tool):
         self.__tools_set.append(tool)
 
     def run(self):
-        self.authenticate()
-        if self.__authentication_status == False:
-            st.error('Username/password is incorrect')
-            return
-        if self.__authentication_status == None:
-            st.warning('Please enter your username and password')
+        if not self.__login():
             return
 
-        st.markdown(
-            """
-            <style>
-            .css-1adrfps {
-                background-color: #6ec0f2;
-                padding: 1rem 1rem;
-            }
-            .css-18e3th9 {
-                padding: 4rem 4rem 5rem 5rem;
-            }
-            </style>
-        """,
-            unsafe_allow_html=True,
-        )
+        # st.markdown(
+        #     """
+        # <style>
+        # [data-testid=stSidebar] :first-child {
+        #     background-color: #6ec0f2;
+        #     color: white;
+        # }
+        # </style>
+        # """,
+        #     unsafe_allow_html=True,
+        # )
 
         # menu picture
         # st.write(os.path.abspath('.'))
@@ -98,27 +126,11 @@ class DataToolsApp:
             一个简单实用的数据科学工具集，左侧菜单是数据工具列表，在此之前您需要选择一个数据集，才能开启数据探索之旅！
             """)
 
-        # data_set
-        data_panel = st.expander("数据源", True)
-        # uploaded_file = data_panel.file_uploader('选择一个数据文件', type='csv')
-
-        orgin_data = pd.read_csv('/data/data_science_app/test_sample.csv')
-        # if uploaded_file is not None:
-        #     data = pd.read_csv(uploaded_file)
-        #     for tools in self.__tools_set:
-        #         tools.use_tool(data)
-        # data = pd.read_csv('/data/data_science_app/test_sample.csv')
-        sql = data_panel.text_area('SQL', 'select * from orgin_data')
-        try:
-            data = sqldf(sql, locals())
-        except:
-            st.error('无效的查询')
-            return
-        data_panel.write(data)
-
-        for tools in self.__tools_set:
-            tools.use_tool(data)
-        self.__authenticator.logout('Logout', 'main')
+        # app_tools
+        data = self.__get_data()
+        if data is not None:
+            for tools in self.__tools_set:
+                tools.use_tool(data)
 
 
 app = DataToolsApp()
@@ -126,3 +138,43 @@ app.add_tool(FeatureTools())
 app.add_tool(StatsTools())
 
 app.run()
+
+# bootstrap 4 collapse example
+# components.html(
+#     """
+#     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+#     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+#     <div id="accordion">
+#       <div class="card">
+#         <div class="card-header" id="headingOne">
+#           <h5 class="mb-0">
+#             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+#             Collapsible Group Item #1
+#             </button>
+#           </h5>
+#         </div>
+#         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+#           <div class="card-body">
+#             Collapsible Group Item #1 content
+#           </div>
+#         </div>
+#       </div>
+#       <div class="card">
+#         <div class="card-header" id="headingTwo">
+#           <h5 class="mb-0">
+#             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+#             Collapsible Group Item #2
+#             </button>
+#           </h5>
+#         </div>
+#         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+#           <div class="card-body">
+#             Collapsible Group Item #2 content
+#           </div>
+#         </div>
+#       </div>
+#     </div>
+#     """,
+#     height=600,
+# )
